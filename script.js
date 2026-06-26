609f26b6c0da3c11532ddf1606ed6a3f3873b4c5
chetanrkembhavi <164353726+chetanrkembhavi@users.noreply.github.com>
2026-06-26 15:29:47 +0530

    Add live product search functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+// ===============================
+// Live Product Search
+// ===============================
+
+function searchProducts() {
+
+    let input = document
+        .getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    let products = document.querySelectorAll(".product-card");
+
+    products.forEach(function(product){
+
+        let name = product.querySelector("h3")
+        .innerText
+        .toLowerCase();
+
+        if(name.includes(input))
+            product.style.display="block";
+        else
+            product.style.display="none";
+
+    });
+
+}
